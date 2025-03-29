09c9602dc9393019a61f5d8202d8ea520562fe20
aagun1234 <155512587+aagun1234@users.noreply.github.com>
2025-03-29 17:50:01 +0800

    Add files via upload

--- httproxy.go
+++ httproxy.go
@@ -19,6 +19,8 @@ import (
 type Config struct {
     listenPort string
     targetURL  string
+	debugLevel int
+	delimiter  string	
     reqHeader  []string
     reqBody    []string
     respHeader []string
@@ -26,7 +28,7 @@ type Config struct {
 }
 
 type RegexReplacement struct {
-	id	int
+	id          int
     Pattern     *regexp.Regexp
     Replacement string
     Count       int // -1 表示替换所有匹配项
@@ -39,9 +41,6 @@ var (
     reqBodyRules     []RegexReplacement
     respHeaderRules  []RegexReplacement
     respBodyRules    []RegexReplacement
-
-	DEBUG	*int
-
 )
 
 func customUsage() {
@@ -49,12 +48,12 @@ func customUsage() {
     fmt.Fprintf(os.Stderr, "简单HTTP反向代理\n用法：%s [参数...]\n", os.Args[0])
     flag.PrintDefaults()
     fmt.Fprintf(os.Stderr, "\n示例:\n  %s -listen :8080 -target https://wishub-x1.ctyun.cn -req-body \"\\\"stream\\\":?true::\\\"stream\\\": false\" -resp-body \"\\\"message\\\"::\\\"delta\\\"\"\n", os.Args[0])
-    fmt.Fprintf(os.Stderr, "  %s -listen :8080 -target https://wishub-x1.ctyun.cn -resp-body \"data:{::data: {\"\n", os.Args[0])
 }
 
 func main() {
     flag.Usage = customUsage
-	DEBUG = flag.Int("debug", 1, "输出调试信息等级")
+	flag.IntVar(&config.debugLevel,"debug", 2, "输出debug信息")
+    flag.StringVar(&config.delimiter,"delim", "\n\n", "流模式chunk分隔符")
     flag.StringVar(&config.listenPort, "listen", ":8080", "本地HTTP侦听端口 (e.g., :8080)")
     flag.StringVar(&config.targetURL, "target", "", "反向代理目标 (e.g., https://wishub-x1.ctyun.cn)")
     flag.Func("req-header", "请求头替换规则 (key:pattern:replacement)", func(s string) error {
@@ -91,8 +90,10 @@ func main() {
     parseRules(config.reqBody, &reqBodyRules, false)
     parseRules(config.respHeader, &respHeaderRules, true)
     parseRules(config.respBody, &respBodyRules, false)
+	config.delimiter=unescapeString(config.delimiter)
 
-	if *DEBUG>=3 { 
+	if config.debugLevel>=4 { 
+		fmt.Printf("DEBUG Chunk delimiter : %s\n",config.delimiter)
 		fmt.Printf("DEBUG req-header : %v\n",reqHeaderRules)
 		fmt.Printf("DEBUG req-body : %v\n",reqBodyRules)
 		fmt.Printf("DEBUG resp-header : %v\n",respHeaderRules)
@@ -102,15 +103,18 @@ func main() {
     // 创建反向代理
     proxy := &httputil.ReverseProxy{
         Director: func(req *http.Request) {
-
-            if *DEBUG>=2 {
-                log.Println("=== 收到的请求头 ===")
-				fmt.Printf("PROTO: %s\n", req.Proto)
-				fmt.Printf("METHOD: %s\n", req.Method)
+			realIP := req.Header.Get("X-Real-IP"); 
+			if realIP == "" {
+				realIP=req.RemoteAddr
+			}
+            if config.debugLevel>=3 {
+                log.Printf("=== 收到%s请求头 ===\n",realIP)
+				fmt.Printf("协议: %s\n", req.Proto)
+				fmt.Printf("方法: %s\n", req.Method)
 				fmt.Printf("HOST: %s\n", req.Host)
 				fmt.Printf("URI: %s\n", req.RequestURI)
                 req.Header.Write(os.Stdout)
-                fmt.Println()
+                fmt.Println("=========")
             }
             req.URL.Scheme = target.Scheme
             req.URL.Host = target.Host
@@ -118,34 +122,38 @@ func main() {
 
             if len(reqHeaderRules) > 0 {
                 modifyHeaders(req.Header, reqHeaderRules)
-                if *DEBUG>=2 {
+                if config.debugLevel>=3 {
                     fmt.Println("=== 修改后的请求头 ===")
-					fmt.Printf("PROTO: %s\n", req.Proto)
-					fmt.Printf("METHOD: %s\n", req.Method)
+					fmt.Printf("协议: %s\n", req.Proto)
+					fmt.Printf("方法: %s\n", req.Method)
 					fmt.Printf("HOST: %s\n", req.Host)
 					fmt.Printf("URI: %s\n", req.RequestURI)
-                   req.Header.Write(os.Stdout)
-                    fmt.Println()
+					req.Header.Write(os.Stdout)
+                    fmt.Println("=========")
                 }
             } else {
-                if *DEBUG>=2 {
+                if config.debugLevel>=3 {
                     fmt.Println("=== 请求头无需修改 ===")
-                    fmt.Println()
+
                 }
             }
-			
+
         },
         ModifyResponse: func(resp *http.Response) error {
-
+			req := resp.Request
+			realIP := req.Header.Get("X-Real-IP"); 
+			if realIP == "" {
+				realIP=req.RemoteAddr
+			}
             // 如果是流式响应，使用自定义 Body 处理
             if resp.Header.Get("Transfer-Encoding") == "chunked" || strings.Contains(resp.Header.Get("Content-Type"), "event-stream") {
                 resp.Body = &streamingBody{
                     reader:    resp.Body,
-					debug:	*DEBUG,
                     rules:     respBodyRules,
                     origBody:  new(bytes.Buffer),
                     totalSize: 0,
-
+					foundDelim: false,
+					clientIP:    realIP,
                 }
             } else {
                 // 非流式响应，整体修改
@@ -155,25 +163,26 @@ func main() {
                 }
                 resp.Body.Close()
 
-                if *DEBUG>=2 {
+                if config.debugLevel>=3 {
                     log.Println("=== 收到的应答体 ===")
-                    body1, _ := replaceStrBytes(body, "\n\n", "**\n\n")
-                    fmt.Println(string(body1))
+                    fmt.Println(string(body))
                     fmt.Println("=========")
+					if config.debugLevel>=4 { fmt.Println(hexDump(body)) }
                 }
 
                 modifiedBody := body
                 if len(respBodyRules) > 0 {
                     modifiedBody = modifyContent(body, respBodyRules)
-                    if *DEBUG>=2 {
+                    if config.debugLevel>=3 {
                         fmt.Println("=== 修改后的应答体 ===")
                         fmt.Println(string(modifiedBody))
                         fmt.Println("=========")
+						if config.debugLevel>=4 { fmt.Println(hexDump(modifiedBody)) }
                     }
                 } else {
-                    if *DEBUG>=2 {
+                    if config.debugLevel>=3 {
                         fmt.Println("=== 应答体无需修改 ===")
-                        fmt.Println()
+
                     }
                 }
 
@@ -181,24 +190,26 @@ func main() {
                 resp.ContentLength = int64(len(modifiedBody))
             }
 			
-            if *DEBUG>=2 {
+            if config.debugLevel>=3 {
                 log.Println("=== 收到的应答头 ===")
-				fmt.Printf("STATUS: %s\n", resp.Status)
-				fmt.Printf("PROTO: %s\n", resp.Proto)
+				fmt.Printf("应答状态: %s\n", resp.Status)
+				fmt.Printf("协议: %s\n", resp.Proto)
                 resp.Header.Write(os.Stdout)
                 fmt.Println("=========")
             }
             if len(respHeaderRules) > 0 {
                 modifyHeaders(resp.Header, respHeaderRules)
-                if *DEBUG>=2 {
+                if config.debugLevel>=3 {
                     fmt.Println("=== 修改后的应答头 ===")
-                    resp.Header.Write(os.Stdout)
+					fmt.Printf("应答状态: %s\n", resp.Status)
+					fmt.Printf("协议: %s\n", resp.Proto)
+					resp.Header.Write(os.Stdout)
                     fmt.Println("=========")
                 }
             } else {
-                if *DEBUG>=2 {
+                if config.debugLevel>=3 {
                     fmt.Println("=== 应答头无需修改 ===")
-                    fmt.Println()
+
                 }
             }
 			
@@ -211,32 +222,35 @@ func main() {
     }
 
     handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		realIP := r.Header.Get("X-Real-IP"); 
+		if realIP == "" {
+			realIP=r.RemoteAddr
+		}
         body, err := io.ReadAll(r.Body)
         if err != nil {
             http.Error(w, "请求体出错", http.StatusInternalServerError)
             return
         }
         r.Body.Close()
-        if *DEBUG>=2 {
-            log.Println("=== 收到的请求体 ===")
+        if config.debugLevel>=3 {
+            log.Printf("=== 收到%s的请求体 ===\n",realIP)
             fmt.Println(string(body))
             fmt.Println("=========")
-			if *DEBUG>=3 { fmt.Println(hexDump(body)) }
+			if config.debugLevel>=4 { fmt.Println(hexDump(body)) }
         }
 
         modifiedBody := body
         if len(reqBodyRules) > 0 {
             modifiedBody = modifyContent(body, reqBodyRules)
-            if *DEBUG>=2 {
+            if config.debugLevel>=3 {
                 fmt.Println("=== 修改后的请求体 ===")
                 fmt.Println(string(modifiedBody))
                 fmt.Println("=========")
- 				if *DEBUG>=3 { fmt.Println(hexDump(modifiedBody))}
+ 				if config.debugLevel>=4 { fmt.Println(hexDump(modifiedBody))}
            }
         } else {
-            if *DEBUG>=2 {
+            if config.debugLevel>=3 {
                 fmt.Println("=== 请求体无需修改 ===")
-                fmt.Println()
             }
         }
         r.Body = io.NopCloser(bytes.NewReader(modifiedBody))
@@ -253,8 +267,7 @@ func main() {
         proxy.ServeHTTP(&streamingResponseWriter{
             ResponseWriter: w,
             flusher:        flusher,
-			debug:	*DEBUG,
-			
+			clientIP:   realIP,
         }, r)
     })
 
@@ -262,58 +275,131 @@ func main() {
     log.Fatal(http.ListenAndServe(config.listenPort, handler))
 }
 
-// streamingBody 自定义 Body，用于流式读取、修改和输出
-type streamingBody struct {
-    reader    io.ReadCloser
-	debug		int
-    rules     []RegexReplacement
-    origBody  *bytes.Buffer // 记录原始完整内容
-    totalSize int           // 记录总大小
-}
-
 func hexDump(data []byte) string {
 	return hex.Dump(data)
 }
+func unescapeString(s string) string {
+	// 替换常见的转义序列
+	s = strings.ReplaceAll(s, `\t`, "\t")
+	s = strings.ReplaceAll(s, `\n`, "\n")
+	s = strings.ReplaceAll(s, `\r`, "\r")
+	s = strings.ReplaceAll(s, `\\`, "\\")
+	s = strings.ReplaceAll(s, `\"`, "\"")
+	return s
+}
 
+// streamingBody 自定义 Body，用于流式读取、修改和输出
+type streamingBody struct {
+    reader    io.ReadCloser
+    rules     []RegexReplacement
+    origBody  *bytes.Buffer // 记录原始完整内容
+    totalSize int           // 记录总大小
+	buffer      []byte        // 内部缓冲区
+    foundDelim bool          // 是否已找到分隔符
+	clientIP string
+}
 
 func (s *streamingBody) Read(p []byte) (n int, err error) {
+	// 如果已经找到双换行符，直接返回缓冲区的数据
+    if s.foundDelim && len(s.buffer) > 0 {
+		//s.buffer中为到分隔符位置的chunk部分内容
+		n = copy(p, s.buffer)
+		s.buffer = s.buffer[n:]
+		if config.debugLevel>=3 {
+            log.Printf("=== 从远端收到的流式应答内容 (chunk 大小: %d 字节) ===\n", n)
+            fmt.Println(string(p[:n]))
+			fmt.Println("=========")
+			if config.debugLevel>=4 { fmt.Println(hexDump(p[:n])) }
+        }
+		// 修改 chunk 内容
+        if len(s.rules)>0 {
+			modifiedChunk := modifyContent(p[:n], s.rules)
+			n=copy(p, modifiedChunk)
+            if config.debugLevel>=3 {
+				fmt.Println("=== 修改后的应答体 ===")
+				fmt.Println(string(modifiedChunk))
+				fmt.Println("=========")
+				if config.debugLevel>=4 { fmt.Println(hexDump(modifiedChunk)) }
+			}
+			if n!=len(modifiedChunk) {
+				log.Printf("奇怪，修改后内容长度与复制缓冲区的长度不一致\n")
+			}
+		} else {
+			if config.debugLevel>=3 {
+				fmt.Println("=== 应答体无需修改 ===")
+
+			}
+		}
+
+        return n, nil
+    }
+	
     n, err = s.reader.Read(p)
     if n > 0 {
         s.origBody.Write(p[:n]) // 记录原始内容
         s.totalSize += n
 
-        if s.debug>=2 {
-            log.Printf("=== 从远端收到的流式应答内容 (chunk 大小: %d 字节) ===\n", n)
-            fmt.Println(string(p[:n]))
-			fmt.Println("=========")
-			if s.debug>=3 { fmt.Println(hexDump(p[:n])) }
+        // 将新数据追加到缓冲区
+        s.buffer = append(s.buffer, p[:n]...)
+
+        // 检查缓冲区中是否有分隔符
+        if idx := bytes.Index(s.buffer, []byte(config.delimiter)); idx != -1 {
+            s.foundDelim = true
+            // 截取到双换行符后的位置，s.buffer中为到分隔符位置的chunk部分内容
+            s.buffer = s.buffer[:idx+len(config.delimiter)]
         }
 
-        // 修改 chunk 内容
-		if len(s.rules)>0 {
-			modifiedChunk := modifyContent(p[:n], s.rules)
-
-			copy(p, modifiedChunk)
-            if s.debug>=2 {
-				fmt.Println("=== 修改后的应答体 ===")
-				fmt.Println(string(modifiedChunk))
+        // 如果找到双换行符或者读取结束，处理并输出
+        if s.foundDelim || err == io.EOF {
+		
+			n = copy(p, s.buffer)
+			s.buffer = s.buffer[n:]
+			if config.debugLevel>=3 {
+				log.Printf("=== 从远端收到的流式应答内容 (chunk 大小: %d 字节) ===\n", n)
+				fmt.Println(string(p[:n]))
 				fmt.Println("=========")
-				if s.debug>=3 { fmt.Println(hexDump(modifiedChunk)) }
+				if config.debugLevel>=4 { fmt.Println(hexDump(p[:n])) }
 			}
-			n=len(modifiedChunk)
-        } else {
-			if s.debug>=2 {
-				fmt.Println("=== 应答体无需修改 ===")
-				fmt.Println()
+			// 修改 chunk 内容
+			if len(s.rules)>0 {
+				modifiedChunk := modifyContent(p[:n], s.rules)
+				n=copy(p, modifiedChunk)
+				if config.debugLevel>=3 {
+					fmt.Println("=== 修改后的应答体 ===")
+					fmt.Println(string(modifiedChunk))
+					fmt.Println("=========")
+					if config.debugLevel>=4 { fmt.Println(hexDump(modifiedChunk)) }
+				}
+				if n!=len(modifiedChunk) {
+					log.Printf("奇怪，修改后内容长度与复制缓冲区的长度不一致\n")
+				}
+			} else {
+				if config.debugLevel>=3 {
+					fmt.Println("=== 应答体无需修改 ===")
+
+				}
 			}
+			return n, nil
 		}
     }
-    return n, err
+	
+	// 如果没有找到双换行符且没有错误，返回0,nil让调用者继续读取
+    if !s.foundDelim && err == nil {
+        return 0, nil
+    }
+
+    // 返回缓冲区的数据
+    if len(s.buffer) > 0 {
+        n = copy(p, s.buffer)
+        s.buffer = s.buffer[n:]
+        return n, err
+    }
+    return 0, err
 }
 
 func (s *streamingBody) Close() error {
-    if s.debug>=1 {
-        fmt.Println("=== 流式应答接收完成，总大小: ", s.totalSize, "字节 ===\n")
+    if config.debugLevel>=1 {
+        fmt.Printf("=== 到%s流式应答完成，总大小: %d字节 ===\n\n",s.clientIP, s.totalSize)
     }
     return s.reader.Close()
 }
@@ -322,15 +408,18 @@ func (s *streamingBody) Close() error {
 type streamingResponseWriter struct {
     http.ResponseWriter
     flusher http.Flusher
-	debug 	int
+	clientIP string
 }
 
 func (w *streamingResponseWriter) Write(b []byte) (int, error) {
     n, err := w.ResponseWriter.Write(b)
     if n > 0 && w.flusher != nil {
         w.flusher.Flush() // 实时推送给客户端
-        if w.debug>=1 {
-            log.Printf("=== 向客户端发送应答内容 (%d 字节) ===\n", n)
+        if config.debugLevel>=2 {
+            log.Printf("=== 向客户端%s发送应答 (%d 字节) ===\n", w.clientIP,n)
+        }
+        if config.debugLevel>=4 {
+            fmt.Println(hexDump(b)) 
         }
     }
     return n, err
@@ -386,9 +475,9 @@ func parseRules(rules []string, target *[]RegexReplacement, isHeader bool) {
         pattern = strings.ReplaceAll(pattern, `\?`, "[.\\s]")
         pattern = strings.ReplaceAll(pattern, `\\n`, "\\n")
 		replacement=strings.ReplaceAll(replacement, `\n`, "\n")
-        if *DEBUG>=3 { log.Printf("DEBUG 正则匹配 %s，替换 %s\n", pattern, replacement) }
-
+        if config.debugLevel>=4 { log.Printf("DEBUG 正则匹配 %s，替换 %s\n", pattern, replacement) }
 		
+ 		
         regex, err := regexp.Compile(pattern)
         if err != nil {
             log.Printf("字符串匹配异常 %s: %v", pattern, err)
@@ -405,22 +494,23 @@ func parseRules(rules []string, target *[]RegexReplacement, isHeader bool) {
     }
 }
 
+
 // 修改headers
 func modifyHeaders(headers http.Header, rules []RegexReplacement) {
     for _, rule := range rules {
-		if *DEBUG>=3 { log.Printf("DEBUG Checking Header rule: %v", rule)}
+		if config.debugLevel>=4 { log.Printf("DEBUG 匹配规则: %v", rule)}
         if rule.HeaderKey == "" {
             continue
         }
         values, ok := headers[rule.HeaderKey]
-		if *DEBUG>=3 { log.Printf("DEBUG Matched Header values: %v", values)}
+		if config.debugLevel>=4 { log.Printf("DEBUG 修改前的头部信息: %v", values)}
         if !ok {
-			if *DEBUG>=3 { log.Printf("DEBUG Add Header %s : %v", rule.HeaderKey, rule.Replacement)}
+			if config.debugLevel>=4 { log.Printf("DEBUG 增加头部 %s : %v", rule.HeaderKey, rule.Replacement)}
 			headers.Set(rule.HeaderKey,rule.Replacement)
             continue
         }
 		if rule.Replacement=="" {
-			if *DEBUG>=3 { log.Printf("DEBUG Delete Header %s", rule.HeaderKey)}
+			if config.debugLevel>=4 { log.Printf("DEBUG 删除头部 %s", rule.HeaderKey)}
 			headers.Del(rule.HeaderKey)
 			continue
 		}
@@ -438,7 +528,7 @@ func modifyHeaders(headers http.Header, rules []RegexReplacement) {
                 })
             }
         }
-		if *DEBUG>=3 { log.Printf("Modified DEBUG Header %s values: %v", rule.HeaderKey, values)}
+		if config.debugLevel>=4 { log.Printf("DEBUG 头部信息修改 %s values: %v", rule.HeaderKey, values)}
         headers[rule.HeaderKey] = values
     }
 }
